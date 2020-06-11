4b2c6498675dc75f671d9505e855482568d67501
Maxopoly <maxopoly3@gmail.com>
2020-06-11 22:19:54 +0200

    First stab at groovy libraries

---
+++ vars/GlobalVariables.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+
+class GlobalVars {
+   static String MVN_VERSION = 'Maven 3.6.3'
+   static String JAVA_VERSION = 'Java 8'
+}
---
+++ vars/civ_discord_send.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+
+def call() {
+     withCredentials([string(credentialsId: 'civclassic-discord-webhook', variable: 'DISCORD_WEBHOOK')]) {
+                discordSend description: "Build: [${currentBuild.id}](${env.BUILD_URL}) **||**  Status: ${currentBuild.currentResult} **||**  [**LOG**](${env.BUILD_URL}consoleFull)\n**Checkstyle warnings:** ${tm('$ANALYSIS_ISSUES_COUNT')}\n", footer: 'Civclassic Jenkins', link: env.BUILD_URL, successful: currentBuild.resultIsBetterOrEqualTo('SUCCESS'), title: "${env.JOB_NAME} #${currentBuild.id}", webhookURL: DISCORD_WEBHOOK            }
+}
