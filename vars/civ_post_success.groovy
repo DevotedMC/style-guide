10087fde0257cae94276f5f308693c08c9606098
Maxopoly <maxopoly3@gmail.com>
2020-06-11 23:53:31 +0200

    null check dependents

--- vars/civ_post_success.groovy
+++ vars/civ_post_success.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-	if (env.BRANCH_NAME == 'master') {
+	if (env.BRANCH_NAME == 'master' && env.civ_dependent_plugins != null) {
 		String [] subPlugins = env.civ_dependent_plugins.split(' ');
 		for(String pluginName : subPlugins) {
 			if (pluginName != '') {
