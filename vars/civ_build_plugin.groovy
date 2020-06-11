b6acec885b4d42de91bb4f1745a44fdbe20834a0
Maxopoly <maxopoly3@gmail.com>
2020-06-11 22:47:32 +0200

    More steps

---
+++ vars/civ_build_plugin.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+
+def call() {
+	if (env.BRANCH_NAME.startsWith('PR')) {
+		sh 'mvn -U clean package -P civ-jenkins'
+	} 
+	else {
+		sh 'mvn -U clean install deploy -P civ-jenkins'
+	} 
+}
+
