d109960438dc86d7941c776a45f89245e77e2e6e
Maxopoly <maxopoly3@gmail.com>
2020-12-30 19:05:45 +0100

    Properly escape quotes

--- vars/civ_archive_artifacts.groovy
+++ vars/civ_archive_artifacts.groovy
@@ -6,7 +6,7 @@ def call() {
     archiveArtifacts artifacts: "target/${projectName}-*.jar", fingerprint: true
     dir ('/var/jenkins_home/repoStaging') {
       sh 'git add .'
-      sh 'git commit -m "${projectName}"'
+      sh 'git commit -m \\"${projectName}\\"'
       sshagent(['d0b8e457-9b94-4222-bf26-a4657960c6d7']) {
         sh 'git push'
       }
