5f881910e82e48a1a07e70b786737c478e4afa67
Maxopoly <maxopoly3@gmail.com>
2020-12-30 19:01:05 +0100

    Use straight git instead of Maven plugin

--- vars/civ_archive_artifacts.groovy
+++ vars/civ_archive_artifacts.groovy
@@ -4,4 +4,11 @@ def call() {
     def allJob = env.JOB_NAME.tokenize('/') as String[];
     def projectName = allJob[1];
     archiveArtifacts artifacts: "target/${projectName}-*.jar", fingerprint: true
+    dir ('/var/jenkins_home/repoStaging') {
+      sh 'git add .'
+      sh 'git commit -m "${projectName}"'
+      sshagent(['d0b8e457-9b94-4222-bf26-a4657960c6d7']) {
+        sh 'git push'
+      }
+    }
 }
